da7ce92eb0da13f65d4e75f1d26a8f323528e821
Tomas <Tomas70333481@gmail.com>
2024-11-28 19:03:00 +0000

    feat: Register service worker

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = "spudis";
+const urlsToCache = [
+  "/",
+  "/index.html",
+];
+
+// Install Service Worker
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Activate Service Worker
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch Cached Files
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
